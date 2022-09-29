219a9b9ec9a6b73b13b831ce585c8d1e891a4204
Hayaty Jayy <113877247+HayatyJ@users.noreply.github.com>
2022-09-29 12:47:37 +0800

    Update 196-delete-duplicate-emails.sql

--- 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
@@ -5,3 +5,5 @@ DELETE FROM Person WHERE id NOT IN
     (SELECT MIN(id) FROM Person GROUP BY email) AS A)
     
 #SELECT * FROM (CONDITION) AS A (TEMP) CODE IS BECAUSE WE NEED TO NESTED IT TWICE BCOZ CANNOT MODIFY SAME TABLE WHEN WE USE SELECT PART
+#mysql does not allow one to use a subquery with a table that is currently being updated in the outer query so we trick it by using it 
+#indirectly like so select * from (some_temp_table) as alias table
